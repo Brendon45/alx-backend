f74bb236f914859c485953056737e47d9ef1fa03
Brendon45 <jejebrendon722@gmail.com>
2024-08-01 20:47:26 +0200

    Added solution to Task 4. Force locale with URL parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,43 +1,37 @@
 #!/usr/bin/env python3
-
 """
-Implementing a way to force a particular locale by passing the 'locale=fr'
-parameter to my app's URLs
+Module 0-app
 """
-
-from typing import Union
-
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-from config import Config
+
+class Config(object):
+    """i18n configuration"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
 babel = Babel(app)
 
-app.config.from_object(Config)
 
-
-@babel.localeselector
-def get_locale() -> Union[str, None]:
-    """
-    Gets locale from a request
-    """
-
-    locale = request.args.get("locale")
-    if locale and locale in Config.LANGUAGES:
-        return locale
-    return request.accept_languages.best_match(Config.LANGUAGES)
-
-
-@app.route("/", methods=["GET"], strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def index():
-    """
-    The index page route, renders the '4-index.html' template
-    """
-
+    """didplays a basic hello world message"""
     return render_template("4-index.html")
 
 
+@babel.localeselector
+def get_locale():
+    """Gets best fmatch locale according to request"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host="0.0.0.0", port="5000")
